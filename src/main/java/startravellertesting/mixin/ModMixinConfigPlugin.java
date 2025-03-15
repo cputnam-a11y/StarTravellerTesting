d56a3aa8ace8ef7376115f26aa294c0c63400aa6
cputnam-a11y <chrlsvryptnm@gmail.com>
2025-03-15 13:57:28 -0500

    comment more

--- src/main/java/startravellertesting/mixin/ModMixinConfigPlugin.java
+++ src/main/java/startravellertesting/mixin/ModMixinConfigPlugin.java
@@ -50,23 +50,30 @@ public class ModMixinConfigPlugin implements IMixinConfigPlugin {
 
     @Override
     public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+        // check if this class needs processing
         if (!MIXINS_TO_PROCESS.contains(mixinClassName))
             return;
+        // check if this class is a record. if it isn't, it shouldn't be processed
         if (!RECORD_NAME.equals(targetClass.superName))
             throw new RuntimeException("RecordComponent can only be applied in records");
         for (var field : targetClass.fields) {
+            // check if the field is annotated with our annotation
             if (field.visibleAnnotations == null || field.visibleAnnotations.stream().noneMatch(a -> ANNOTATION_DESC.equals(a.desc))) {
                 continue;
             }
+            // record components may not be static
             if (Modifier.isStatic(field.access)) {
                 throw new RuntimeException("RecordComponent annotation is not allowed on static fields");
             }
+            // remove our annotation
             field.visibleAnnotations.removeIf(a -> ANNOTATION_DESC.equals(a.desc));
+            // add the field as a record component...
             targetClass.recordComponents.add(new RecordComponentNode(
                     field.name,
                     field.desc,
                     field.signature
             ));
+            // technically still missing canonical constructor, but I can't be bothered to care.
             // getter done through mixin
         }
     }
