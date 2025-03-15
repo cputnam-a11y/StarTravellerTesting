9cfebdf58cf897a15ea179358d4432923e9b2451
cputnam-a11y <chrlsvryptnm@gmail.com>
2025-03-15 16:03:02 -0500

    add small example of instantiation

--- src/main/java/startravellertesting/StarTravellerTesting.java
+++ src/main/java/startravellertesting/StarTravellerTesting.java
@@ -1,8 +1,12 @@
 package startravellertesting;
 
+import it.unimi.dsi.fastutil.ints.IntList;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.component.type.FireworkExplosionComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import startravellertesting.duck.FireworkExplosionComponentDuck;
+import startravellertesting.util.ExtraParams;
 
 public class StarTravellerTesting implements ModInitializer {
     public static final String MOD_ID = "startravellertesting";
@@ -11,5 +15,11 @@ public class StarTravellerTesting implements ModInitializer {
     @Override
     public void onInitialize() {
         LOGGER.info("Hello Fabric world!");
+        ExtraParams params = new ExtraParams(false);
+        var component = FireworkExplosionComponentDuck.doWithContext(
+                params,
+                () -> new FireworkExplosionComponent(FireworkExplosionComponent.Type.BURST, IntList.of(1), IntList.of(3), true, true)
+        );
+
     }
 }
